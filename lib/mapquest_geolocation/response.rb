4bea2d56f6d717c6e13219d6f8f60deb8aaf3fcc
Gordan Grasarevic <me@ggordan.com>
2013-05-08 22:46:04 +0100

    Updated response class

--- lib/mapquest_geolocation/response.rb
+++ lib/mapquest_geolocation/response.rb
@@ -12,10 +12,43 @@ module MapquestGeolocation
     end
 
     def locations
-      []
-      unless empty?
-        response['results'].first['locations']
+      if empty?
+        raise Error
+      else
+        locations = []
+        response['results'].first['locations'].each { |k| locations.push sym k }
+        return locations
       end
+    end
+
+    def providedLocation
+      unless empty?
+        sym response['results'].first['providedLocation']
+      end
+    end
+
+    def options
+      sym response['options']
+    end
+
+    def info
+      sym response['info']
+    end
+
+    def status
+      return { :code => info[:statuscode], :messages => info[:messages] }
+    end
+
+    def sym(hash)
+      tmp = {}
+      hash.each do |k,v|
+        if v.is_a? Hash
+          tmp[k.to_sym] = sym v
+        else
+          tmp[k.to_sym] = v
+        end
+      end
+      return tmp
     end
 
   end
