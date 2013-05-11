e7ae924598f7054ed0abeb11750a551d559b3690
Gordan Grasarevic <me@ggordan.com>
2013-05-11 22:59:58 +0100

    updated directions to reflect changes

--- lib/mapquest/services/directions.rb
+++ lib/mapquest/services/directions.rb
@@ -2,9 +2,16 @@ class MapQuest
   module Services
     class Directions < Core
 
+      API_LOCATION = :directions
+
       def get(params)
         if params.has_key? :from && :to
-          response = mapquest.request :directions, params, Response
+          api_method = {
+              :location => API_LOCATION,
+              :version => '1',
+              :call => 'route'
+          }
+          response = mapquest.request api_method, params, Response
         else
           raise Error
         end
@@ -12,8 +19,39 @@ class MapQuest
 
       class Response < MapQuest::Response
 
+        def valid_request?
+        end
+
         def route
           response[:route]
+        end
+
+        # Returns the drive time in <b>minutes</b>
+        def time
+          (route[:time].to_i / 60).ceil
+        end
+
+        # Returns the calculated distance of the route in <b>miles</b>
+        def distance
+          route[:distance].to_i
+        end
+
+        def locations
+          route[:locations]
+        end
+
+        # Returns a hash of the maneuvers in the route
+        def maneuvers
+          route[:legs].first[:maneuvers]
+        end
+
+        # Returns only the narratives for the route as a list
+        def narrative
+          narrative = []
+          route[:legs].first[:maneuvers].each do |maneuver|
+            narrative.push maneuver[:narrative]
+          end
+          narrative
         end
 
       end
