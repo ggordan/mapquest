ae5ad41a1b7d9e1e30019cb4c3ce39e4f08a0f83
Gordan Grasarevic <me@ggordan.com>
2013-05-11 18:55:05 +0100

    Changed service access hash

--- lib/mapquest/request.rb
+++ lib/mapquest/request.rb
@@ -2,17 +2,17 @@ class MapQuest
   class Request < RestClient::Resource
 
     # The base url of the mapquest api
-    API_ROOT = 'http://www.mapquestapi.com/%s/v1/%s'
+    API_ROOT = 'http://www.mapquestapi.com/%s/v%s/%s'
 
     ACCESS = {
-        :geocoding => 'address',
-        :directions => 'route',
-        :search => 'search',
-        :staticmap => 'getmap',
+        :geocoding => { :v => '1', :call => 'address'},
+        :directions => { :v => '1', :call => 'route'},
+        :search => { :v => '2', :call => 'search'},
+        :staticmap => { :v => '4', :call => 'staticmap'},
     }
 
     def initialize(method)
-      super API_ROOT % [method.to_s, ACCESS[method]]
+      super API_ROOT % [method.to_s, ACCESS[method][:v], ACCESS[method][:call]]
     end
 
     def send(params)
