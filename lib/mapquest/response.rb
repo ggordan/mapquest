83e3a10c2333a6e39a7f2de9dbc889527065967f
Gordan Grasarevic <me@ggordan.com>
2013-05-11 01:56:19 +0100

    Moved geocoding specific responses to own class, removed from MapQuest::Request

--- lib/mapquest/response.rb
+++ lib/mapquest/response.rb
@@ -7,26 +7,6 @@ class MapQuest
       @response = JSON.parse(res, :symbolize_names => true)
     end
 
-    def empty?
-      true if response[:results].first[:locations].any? else false
-    end
-
-    def locations
-      unless empty?
-        response[:results].first[:locations]
-      end
-    end
-
-    def providedLocation
-      unless empty?
-        response[:results].first[:providedLocation]
-      end
-    end
-
-    def options
-      response[:options]
-    end
-
     def info
       response[:info]
     end
--- lib/mapquest/services/geocoding.rb
+++ lib/mapquest/services/geocoding.rb
@@ -4,7 +4,31 @@ class MapQuest
 
       def decode(params)
         raise Error unless params.has_key? :location
-        response = mapquest.request :geocoding, params
+        response = mapquest.request :geocoding, params, Response
+      end
+
+      class Response < MapQuest::Response
+
+        def empty?
+          true if response[:results].first[:locations].any? else false
+        end
+
+        def locations
+          unless empty?
+            response[:results].first[:locations]
+          end
+        end
+
+        def providedLocation
+          unless empty?
+            response[:results].first[:providedLocation]
+          end
+        end
+
+        def options
+          response[:options]
+        end
+
       end
 
     end
