3feec56d40706285e83fc17fe4a1ba873579eddf
Gordan Grasarevic <me@ggordan.com>
2013-05-11 22:50:27 +0100

    Updated the spec helper

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -3,7 +3,7 @@ require 'mapquest'
 RSpec.configure do |config|
 
   def fixture(filename)
-    File.open(File.dirname(__FILE__) + '/fixtures/' + filename + '.json', 'r').readlines
+    File.open(File.dirname(__FILE__) + '/fixtures/' + filename + '.json', 'r').read
   end
 
   def init
