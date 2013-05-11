acaac48bcfc77a5b09b58f80b61ba2f2bcbdfbe1
Gordan Grasarevic <me@ggordan.com>
2013-05-11 22:52:28 +0100

    Changed parameters used to call webservice

--- lib/mapquest/request.rb
+++ lib/mapquest/request.rb
@@ -4,15 +4,8 @@ class MapQuest
     # The base url of the mapquest api
     API_ROOT = 'http://www.mapquestapi.com/%s/v%s/%s'
 
-    ACCESS = {
-        :geocoding => { :v => '1', :call => 'address'},
-        :directions => { :v => '1', :call => 'route'},
-        :search => { :v => '2', :call => 'search'},
-        :staticmap => { :v => '4', :call => 'staticmap'},
-    }
-
     def initialize(method)
-      super API_ROOT % [method.to_s, ACCESS[method][:v], ACCESS[method][:call]]
+      super API_ROOT % [method[:location], method[:version], method[:call]]
     end
 
     def send(params)
