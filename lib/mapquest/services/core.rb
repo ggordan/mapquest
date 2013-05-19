f9ff84891da3bfbe88d734c200d7422773e8d1a7
Gordan Grasarevic <me@ggordan.com>
2013-05-19 02:27:47 +0100

    Added call_api method To avoid redeclaring same method in each service class

--- lib/mapquest/services/core.rb
+++ lib/mapquest/services/core.rb
@@ -8,6 +8,16 @@ class MapQuest
         @mapquest = mapquest
       end
 
+      def call_api(api, version, call, options)
+        # Remove invalid options
+        options.keys.select { |k| api.class::VALID_OPTIONS.include? k }
+        api_method = {
+            :location => api.class::API_LOCATION,
+            :version => version,
+            :call => call
+        }
+        mapquest.request api_method, options, api.class::Response
+      end
     end
   end
 end
